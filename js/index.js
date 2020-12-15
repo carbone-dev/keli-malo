f2b6bfb887e09daa215b382143c6689f5a68a500
tsiox dev <tsiox.dev@gmail.com>
2020-12-15 16:18:46 +0300

    corection

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-
+window.onscroll = function (e) {  
+	
+	console.log(window.scrollY);
+	if (window.scrollY > 300){
+		document.getElementById("header-fixe").style.boxShadow = "0 0 20px rgb(0, 0, 0, 0.3)";
+		document.getElementById("header-fixe").style.position = "fixed";
+	}
+	else{
+		document.getElementById("header-fixe").style.boxShadow = "none";
+		document.getElementById("header-fixe").style.position = "inherit";
+	}
+	for (opacity = 0; opacity < 1.1; opacity = opacity + 0.1) 
+{           
+    setTimeout(function(){document.getElementById('header-fixe').style.opacity = opacity;},100)                       
+}  
+};
 document.getElementById('play-vid').onclick = function () {
 	document.getElementById('videos-1').play();
 	document.getElementById('play-vid').style.display = 'none'
@@ -47,19 +62,3 @@ document.getElementById("inscrire-1").style.display = "none";
 document.getElementById("contenu-popup").style.padding = "5px 60px";
 }
 
-window.onscroll = function (e) {  
-	
-	console.log(window.scrollY);
-	if (window.scrollY > 300){
-		document.getElementById("header-fixe").style.boxShadow = "0 0 20px rgb(0, 0, 0, 0.3)";
-		document.getElementById("header-fixe").style.position = "fixed";
-	}
-	else{
-		document.getElementById("header-fixe").style.boxShadow = "none";
-		document.getElementById("header-fixe").style.position = "inherit";
-	}
-	for (opacity = 0; opacity < 1.1; opacity = opacity + 0.1) 
-{           
-    setTimeout(function(){document.getElementById('header-fixe').style.opacity = opacity;},100)                       
-}  
-}
